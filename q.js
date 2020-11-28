e3247656d79edac15de3749b32bb3b827be987f9
Andrew Dear <andrew@kangaroorewards.com>
2020-11-28 02:20:19 -0500

    v2.321
    - Small fixes
    - Added dataURLToBlob
    - File upload rewrite

--- q.js
+++ q.js
@@ -13,7 +13,7 @@
 		return that.put(mixedQuery);
 	},
 
-	version = q.version = 2.320,
+	version = q.version = 2.321,
 	
 	BYPASS_QUEUE = q.BYPASS_QUEUE = 'BYPASS_QUEUE_CONSTANT',
 
@@ -1482,7 +1482,10 @@
 			mixedReplacement = q(mixedReplacement);
 		var that = this,
 		replacement = mixedReplacement.is_qchain ? mixedReplacement[0] : mixedReplacement;
-		that.parent()[0].replaceChild(replacement, that[0]);
+		var parent = that.parent();
+		if (!parent.length)
+			return null;
+		parent[0].replaceChild(replacement, that[0]);
 		that[0] = replacement;
 		that.length = 1;
 		return that;
@@ -1767,8 +1770,7 @@
 		  };
 		}
 		var r = new XMLHttpRequest();
-		r.open((arrParams.post || arrParams.formData) ? "POST" : "GET", arrParams.url);
-		r.withCredentials = false;
+		r.open((arrParams.formData || arrParams.post) ? "POST" : "GET", arrParams.url);
 		if (arrParams.cross)
 			r.withCredentials = true;
 		if (arrParams.encoding !== false)
@@ -1801,7 +1803,103 @@
 			r.send(arrParams.post ? q(arrParams.post).serialize('=', '&', true).replace(/%20/g, '+') : null);
 		return r;
 	};
+	q.dataURLToBlob = function(dataURL) {
+	    var BASE64_MARKER = ';base64,';
+	    if (dataURL.indexOf(BASE64_MARKER) == -1) {
+	        var parts = dataURL.split(',');
+	        var contentType = parts[0].split(':')[1];
+	        var raw = parts[1];
+	
+	        return new Blob([raw], {type: contentType});
+	    }
+	    var parts = dataURL.split(BASE64_MARKER);
+	    var contentType = parts[0].split(':')[1];
+	    var raw = window.atob(parts[1]);
+	    var rawLength = raw.length;
+	
+	    var uInt8Array = new Uint8Array(rawLength);
+	
+	    for (var i = 0; i < rawLength; ++i) {
+	        uInt8Array[i] = raw.charCodeAt(i);
+	    }
+	
+	    return new Blob([uInt8Array], {type: contentType});
+	}
 	q.fileUpload = function (arrParams) {
+		var that = this;
+		$('._q-file-upload-input').remove();
+		var input = q("<input type='file' name='file' class='_q-file-upload-input'>")
+		.css({
+			display : 'none'
+		})
+		.change(function (e) {
+			var file = e.target.files[0];
+			var reader = new FileReader();
+			reader.onload = function (readerEvent) {
+				if (
+					file.type.match(/image.*/)
+					&& arrParams.minMaxSize
+				) {
+					var image = new Image();
+					image.onload = function (imageEvent) {
+						// Resize the image
+						var canvas = document.createElement('canvas'),
+						max_size = arrParams.minMaxSize,
+						width = image.width,
+						height = image.height;
+						if (width > height) {
+							if (width > max_size) {
+								height *= max_size / width;
+								width = max_size;
+							}
+						} else {
+							if (height > max_size) {
+								width *= max_size / height;
+								height = max_size;
+							}
+						}
+						canvas.width = width;
+						canvas.height = height;
+						canvas.getContext('2d').drawImage(image, 0, 0, width, height);
+						var dataUrl = canvas.toDataURL();
+						var resizedImage = q.dataURLToBlob(dataUrl);
+						do_request(file, resizedImage);
+					};
+					image.src = readerEvent.target.result;
+				} else {
+					do_request(file);
+				}
+			};
+			reader.readAsDataURL(file);
+		
+			function do_request(fileInput, alternateData) {
+				var formData = new FormData();
+				formData.append("file", alternateData || fileInput);
+				if (arrParams.selected)
+					arrParams.selected.call(that,fileInput,e);
+				if (arrParams.post) {
+					for (var strKey in arrParams.post) {
+						formData.append(strKey, arrParams.post[strKey]);
+					}
+				}
+				delete arrParams.post;
+				var request = {
+					url : arrParams.url,
+					formData : formData,
+					encoding : false
+				};
+				extend(request, arrParams);
+				//that && that.request ? that.request(request) : q.request(request); // removed because something is interfearing with it and until thats understood it has been taken out
+				q.request.call(that, request);
+				input.remove();
+			}
+		})
+		.appendTo('body');
+		if (arrParams.accept)
+			input.attr('accept', arrParams.accept);
+		input[0].click();
+	};
+	q.fileUpload2 = function (arrParams) {
 		var that = this;
 		$('._q-file-upload-form').remove();
 		var form = q('<form class="_q-file-upload-form">')
